660593832ec7f29138b4459f5088085c60f001e1
Taylor Gulley <taylorlgulley@yahoo.com>
2018-07-17 11:48:24 -0500

    made an arrow function

--- overly-excited/overly-excited.js
+++ overly-excited/overly-excited.js
@@ -17,14 +17,14 @@ function addExcitement (theWordArray, x) {             // added another argument
 
     for (let i = 1; i <= theWordArray.length; i++) {
         if ( i % 3 === 0 && i !== 0) {
-            buildMeUp += " " + sentence[i-1];
+            buildMeUp += " " + theWordArray[i-1];
             for (let j = 1; j <= i; j++) {        // add another counter and for loop to add on ! for each time j is 
                 if ( j % 3 === 0 ) {
                     buildMeUp += x               // change ! to x so when you call the function you can change what punctuation you want
                 }
             }
         } else {
-            buildMeUp += " " + sentence[i-1];
+            buildMeUp += " " + theWordArray[i-1];
         }
        // Concatenate the new word onto buildMeUp
        // Print buildMeUp to the console
@@ -38,10 +38,51 @@ addExcitement(sentence, "?");
 
 
 
+//another method to solve the above problem
+// let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+
+// function addExcitement (theWordArray, symb) {
+
+//     let buildMeUp = "";
+//     let wordCount = 0;
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         wordCount ++;
+//         let currentWord = theWordArray[i];
+//         buildMeUp += " " + currentWord;
+
+//         if(wordCount % 3 === 0){
+//             buildMeUp = buildMeUp + symb.repeat(wordCount/3);
+//         }
+//         console.log(buildMeUp);
+//     }
+
+// }
+
+// addExcitement(sentence, " SO GREAT ");
 
 
 
 
 
+let addMoreExcitement = (theWordArray, x) => {             // the arrow function version of my code above
 
+    let buildMeUp = ""
 
+    for (let i = 1; i <= theWordArray.length; i++) {
+        if ( i % 3 === 0 && i !== 0) {
+            buildMeUp += " " + theWordArray[i-1];
+            for (let j = 1; j <= i; j++) {
+                if ( j % 3 === 0 ) {
+                    buildMeUp += x
+                }
+            }
+        } else {
+            buildMeUp += " " + theWordArray[i-1];
+        }
+        console.log(buildMeUp);
+    } 
+}
+
+addMoreExcitement(sentence, "&");
